1072fe8388539f8b8b821777180e207e078754f3
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-08-31 13:03:41 +0000

    etapa de cadastro

--- gestor_salas.py
+++ gestor_salas.py
@@ -1,1 +1,11 @@
+#Etapa1 - Cadastro das Empresas e seus projetos
+startups = {
+    "nome": "Flare Solution",
+    "etapa": "Aceleracao",
+    "início": "Abril de 2026"
 
+}
+projetos_ativos = ["App Mobile", "Signal Found"]
+
+print("STARTUP:",startups["nome"],"Fase:", startups["etapa"])
+print("Projeto em Andamento:",projetos_ativos[0],"início", startups["início"])
