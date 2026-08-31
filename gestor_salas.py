f55bd80cf53f62e13abd781738c512c5161bd849
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-08-31 14:12:53 +0000

    Etapa De mapeamneto

--- gestor_salas.py
+++ gestor_salas.py
@@ -9,3 +9,16 @@ projetos_ativos = ["App Mobile", "Signal Found"]
 
 print("STARTUP:",startups["nome"],"Fase:", startups["etapa"])
 print("Projeto em Andamento:",projetos_ativos[0],"início", startups["início"])
+
+#Etapa2 - Mapeamento de Salas
+#Sala Ocupada = 1 e Sala Livre =0
+salas = [
+    [1, 0],
+    [0,1]
+
+]
+print("Status da Sala A1", salas[0][0])
+print("Status da Sala A2", salas[0][1])
+print("Status da Sala B1", salas[1][0])
+print("Status da Sala B2", salas[1][1])
+print("Classificacao de Status: 1= Ocupado e 0 = Livre")
