47760143820965402a9021ddfd08a1b558705936
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-08-31 16:00:35 +0000

    etapa de leitura

--- gestor_salas.py
+++ gestor_salas.py
@@ -22,3 +22,17 @@ print("Status da Sala A2", salas[0][1])
 print("Status da Sala B1", salas[1][0])
 print("Status da Sala B2", salas[1][1])
 print("Classificacao de Status: 1= Ocupado e 0 = Livre")
+
+#Etapa3 - Leitura de Dados Operacionais
+with open("dados.csv", "r", encoding="utf-8") as arquivo:
+    cabecalho = arquivo.readline()
+    linha1 = arquivo.readline()
+    linha2 = arquivo.readline()
+    linha3 = arquivo.readline()
+    linha4 = arquivo.readline()
+
+print("Cabecalho:", cabecalho)
+print("Linha 1:", linha1)
+print("Linha 2:", linha2)
+print("Linha 3:", linha3)
+print("Linha 4:", linha4)
